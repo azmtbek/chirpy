0f7f21a8b3013e48860e3109b94719a08f37ca28
Azamatbek Mamarajabov <azamatbek.dev@gmail.com>
2025-06-29 20:00:17 +0500

    change metrics and reset

--- main.go
+++ main.go
@@ -1,22 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
-	"encoding/json"
 	"net/http"
 	"sync/atomic"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
 }
 
 func main() {
@@ -43,27 +36,8 @@ func main() {
 		w.Write([]byte("OK"))
 	})
 
-	mux.HandleFunc("GET /admin/metrics", func(w http.ResponseWriter, r *http.Request) {
-		str := fmt.Sprintf(`<html>
-  		<body>
-    		<h1>Welcome, Chirpy Admin</h1>
-    		<p>Chirpy has been visited %d times!</p>
-  			</body>
-			</html>`, apiConf.fileserverHits.Load())
-
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(str))
-	})
-
-	mux.HandleFunc("POST /admin/reset", func(w http.ResponseWriter, r *http.Request) {
-		apiConf.fileserverHits.Store(0)
-		str := fmt.Sprintf("Hits: %d", apiConf.fileserverHits.Load())
-
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(str))
-	})
+	mux.HandleFunc("GET /admin/metics", apiConf.handlerMetrics)
+	mux.HandleFunc("POST /admin/reset", apiConf.handlerReset)
 
 	mux.HandleFunc("POST /api/validate_chirp", func(w http.ResponseWriter, r *http.Request) {
 		type parameters struct {
